7a5abb372f25d83f156dbc6db4bdacd3bbd1df6a
Frazer <s10222914@connect.np.edu.sg>
2022-12-16 14:11:42 +0800

    More scripts

--- Base URP with Firebase XRTK/Assets/Scripts/GunShoot.cs
+++ Base URP with Firebase XRTK/Assets/Scripts/GunShoot.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class GunShoot : MonoBehaviour
 {
     public Transform bulletOrigin;
-
     public GameObject bulletPrefab;
 
     public void Fire()
     {
         GameObject newBullet = Instantiate(bulletPrefab, bulletOrigin.position, transform.rotation);
-        newBullet.GetComponent<Rigidbody>().AddForce(bulletOrigin.forward * 50f);
+        newBullet.GetComponent<Rigidbody>().AddForce(bulletOrigin.forward * 150f);
     }
 }
--- Base URP with Firebase XRTK/Assets/Scripts/PlayerHealth.cs
+++ Base URP with Firebase XRTK/Assets/Scripts/PlayerHealth.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -58,3 +58,4 @@ public class PlayerHealth : MonoBehaviour
 
     }
 }
+*/
